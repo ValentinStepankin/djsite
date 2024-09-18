31c8e5dede7b960167838c6cea40cd2f421819a1
Stepankin Valentin <vallentinnnnn546@gmail.com>
2024-09-18 23:51:19 +0300

    Add category model

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -9,9 +9,20 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+    cats = models.ForeignKey('Category', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
