ddedda36d5fe3fc5b49b0ec184f318652a373fa3
Valentin Stepankin <vallentinnnnn546@gmail.com>
2024-09-19 13:00:32 +0300

    ---

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -20,8 +20,8 @@ class Women(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
 
-    def __str__(self):
-        return self.title
+    # def __str__(self):
+    #     return self.name
 
 
 
